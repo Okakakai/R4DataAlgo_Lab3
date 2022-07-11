03d7cf62334f3633a314a586e0f5b7bb76bbbbf5
Okakakai <inoue.kai.work@gmail.com>
2022-07-11 21:06:09 +0900

    [追加] delete function

--- trieTree.cpp
+++ trieTree.cpp
@@ -38,24 +38,25 @@ trieTree::node *trieTree::createNode()
 
     for (int i = 0; i < ALPHABET_SIZE; i++)
         pNode->children[i] = NULL;
-
     return pNode;
 }
 
-void trieTree::contains(string target)
+bool trieTree::contains(string target)
 {
     bool found = search(target);
     if (found)
     {
         std::cout << target;
         std::cout << " is in the document" << endl;
+        return true;
     }
     else
     {
         std::cout << "\"" << target << "\" "
-                  << "is not in the document";
+                  << "is not in the document" << endl;
+        ;
+        return false;
     }
-    std::cout << endl;
 }
 void trieTree::insert(string key)
 {
@@ -94,36 +95,36 @@ bool trieTree::search(string key)
     return (x->isEndOfWord);
 }
 
-// void trieTree::printTree()
-// {
-//     printHelper(root);
-// }
+void trieTree::printTree()
+{
+    printHelper(root);
+}
 
-// void trieTree::printHelper(node *x)
-// {
-//     char arphabet = 'a';
-//     if (x->isEndOfWord)
-//     {
-//         for (int j = 0; j < ALPHABET_SIZE; j++)
-//         {
-//             if (root->children[j])
-//             {
-//                 char temp = arphabet + j;
-//                 cout << temp << endl;
-//             }
-//             cout << endl;
-//         }
-//     }
-//     if (!x->isEndOfWord)
-//     {
-//         for (int i = 0; i < ALPHABET_SIZE; i++)
-//         {
-//             printHelper(x->children[i]);
-//             char temp = arphabet + i;
-//             cout << temp << endl;
-//         }
-//     }
-// }
+void trieTree::printHelper(node *x)
+{
+    char arphabet = 'a';
+    if (x->isEndOfWord)
+    {
+        for (int j = 0; j < ALPHABET_SIZE; j++)
+        {
+            if (root->children[j])
+            {
+                char temp = arphabet + j;
+                cout << temp << endl;
+            }
+            cout << endl;
+        }
+    }
+    if (!x->isEndOfWord)
+    {
+        for (int i = 0; i < ALPHABET_SIZE; i++)
+        {
+            printHelper(x->children[i]);
+            char temp = arphabet + i;
+            cout << temp << endl;
+        }
+    }
+}
 
 bool trieTree::isLeafNode(node *node)
 {
@@ -132,6 +133,70 @@ bool trieTree::isLeafNode(node *node)
         if (!node->children[i])
         {
             return false;
+        }
+    }
+    return true;
+}
+
+bool trieTree::isOneChild(node *node)
+{
+    int flag = 0;
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        if (node->children[i])
+        {
+            flag++;
+        }
+        if (flag > 1)
+        {
+            return false;
+        }
+    }
+    if (flag == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool trieTree::deleteNode(string target)
+{
+    struct node *x = root;
+    string keys;
+    for (int i = 0; i < target.length(); i++)
+    {
+        keys.push_back(target[i]);
+    }
+    std::vector<node *> vec = std::vector<node *>();
+    node *tmp = NULL;
+    for (int i = 0; i < target.length(); i++)
+    {
+        if (i == 0)
+        {
+            tmp = x->children[target[i] - 'a'];
+        }
+        else
+        {
+            tmp = tmp->children[target[i] - 'a'];
+        }
+        vec.push_back(tmp);
+    }
+    node *endian = vec[target.length() - 1];
+    if (!isLeafNode(endian))
+    {
+        vec[target.length() - 1]->isEndOfWord = false;
+        return false;
+    }
+    for (int i = target.length() - 1; i >= 0; i--)
+    {
+        if (i != target.length() - 1 && vec.at(i)->isEndOfWord)
+        {
+            break;
+        }
+        if (i == 0 || (!isLeafNode(vec.at(i)) && !isOneChild(vec.at(i))))
+        {
+            free(vec.at(i)->children[target[target.length() - i - 1] - 'a']);
+            break;
         }
     }
     return true;
