68914fd0482e0fb118148a00f7341bd88cc0bacc
Okakakai <inoue.kai.work@gmail.com>
2022-07-11 10:12:15 +0900

    [追加] trieTreeの検索を呼び出し

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "ttTree.h"
+#include "trieTree.h"
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
         cout << "=====================================================" << endl;
         cout << "Options: " << endl;
         cout << "(a) 2-3 Tree " << endl;
-        cout << "(b) ---- " << endl;
+        cout << "(b) trie Tree " << endl;
         cout << "(c) --- " << endl;
         cout << "Choice: ";
         cin >> option;
@@ -58,6 +59,46 @@ int main(int argc, char *argv[])
                     cout << "Delete Num : ";
                     cin >> target;
                     myTree.deleteNode(target);
+                }
+
+                // Quit
+                else
+                    break;
+            }
+        }
+        if (option == 'b')
+        {
+            trieTree trieTree;
+            trieTree.buildTree();
+            while (1)
+            {
+                choice = 0;
+                cout << "Options: (a) display index, (b) search, (c) delete (d) quit\n";
+                cin >> choice;
+
+                // Print index
+                if (choice == 'a')
+                {
+                    trieTree.printTree();
+                }
+                // Search index
+                else if (choice == 'b')
+                {
+                    string word1, word2, word3;
+                    word1 = "card";
+                    word2 = "carp";
+                    word3 = "trie";
+                    trieTree.contains(word1);
+                    trieTree.contains(word2);
+                    trieTree.contains(word3);
+                }
+
+                // delete
+                else if (choice == 'c')
+                {
+                    int target;
+                    cout << "Delete Num : ";
+                    cin >> target;
                 }
 
                 // Quit
